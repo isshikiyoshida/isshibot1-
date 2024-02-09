4f0ab0c0f4ffa75316ae9feaaf13fbb3568f66ab
isshikiyoshida <116609169+isshikiyoshida@users.noreply.github.com>
2024-02-08 22:53:25 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,9 +1,9 @@
 let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-*HOLA TE INVITO A UNIRTE AL GRUPO SUPORT DE TOXI-BOT Y FORMAR PARTE DE LA COMUNIDAD*
+*HOLA TE INVITO A UNIRTE AL GRUPO SUPORT DE IsshiBot Y FORMAR PARTE DE LA COMUNIDAD*
 
 *➤ 𝙶𝚛𝚞𝚙𝚘 𝚘𝚏𝚒𝚌𝚒𝚊𝚕 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-https://chat.whatsapp.com/L2PCWj8QQQ6HPZvmR7izIT* 
+*1.-https://chat.whatsapp.com/Lj9f32H4d3OJcmCQTm6oD5* 
 `.trim(), wm, media, [['💟 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 💟', '#menu']], m)
 handler.command = /^linkgc|grupos$/i
 export default handler
